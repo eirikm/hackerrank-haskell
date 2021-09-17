04fed6b4125eec58447e68b24e5df36bece4284b
Eirik Meland <eirik.meland@gmail.com>
2021-09-17 20:35:55 +0000

    FilterArray with test

---
+++ src/FilterArray.hs
@@ -0,0 +1,15 @@
+module FilterArray where
+
+main :: IO ()
+main = interact (format . filterArray . parse)
+    where
+        format :: [Int] -> String 
+        format ints = unlines $ show <$> ints
+
+        parse :: String -> (Int, [Int])
+        parse s = do
+            let ints = read <$> words s
+            (head ints, tail ints)
+
+filterArray :: (Int, [Int]) -> [Int]
+filterArray (x, bs) = filter (< x) bs
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,16 +2,20 @@
 
 module Main where
 
-import Test.Hspec
-import SolveMeFirst (solveMeFirst)
+import FilterArray (filterArray)
 import ListReplication (listReplication)
+import SolveMeFirst (solveMeFirst)
+import Test.Hspec
 
 main :: IO ()
 main = hspec $
     describe "hackerrank" do
-        context "SolveMeFirst" do 
-            it "test set" do 
+        context "SolveMeFirst" do
+            it "test set" do
                 solveMeFirst "2 3" `shouldBe` 5
-        context "ListReplication" do 
-            it "test set" do 
-                listReplication [3 ,1 ,2 ,3 ,4] `shouldBe` [1 ,1 ,1 ,2 ,2 ,2 ,3 ,3 ,3 ,4 ,4 ,4]
+        context "ListReplication" do
+            it "test set" do
+                listReplication [3, 1, 2, 3, 4] `shouldBe` [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]
+        context "FilterArray" do
+            fit "test set" do
+                filterArray (3, [10, 8, 9, 2, 7, 5, 1, 3, 0]) `shouldBe` [2, 1, 0]
